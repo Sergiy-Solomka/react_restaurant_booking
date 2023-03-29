d453144fcd693d4fff1251eae7e15e02f0fb3379
Sergiy Solomka <ssolomka@gmail.com>
2023-03-29 18:04:40 +0200

    Added some styling and better view of month and events.

--- src/componentes/Calendar/Calendar.tsx
+++ src/componentes/Calendar/Calendar.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import './Calendar.scss'
 
-const events = [{ title: 'Meeting', start: new Date() }]
+const events = [ {
+  //id: '123',
+  title: '25',
+  start: '2023-03-29',
+  allDay: true,
+  color: 'transparent',
+  textColor: 'red',
+}]
 export const Calendar = () => {
   const handleDateClick = (e: any) => {
     console.log(e.date)
@@ -15,6 +23,14 @@ export const Calendar = () => {
       initialView='dayGridMonth'
       dateClick={handleDateClick}
       events={events}
+      height={ '85vh' }
+      headerToolbar={{
+        start: 'prev,next today',
+        center: 'title',
+        end: 'dayGridMonth,dayGridWeek'
+      }}
+
+
     />
   )
 }
