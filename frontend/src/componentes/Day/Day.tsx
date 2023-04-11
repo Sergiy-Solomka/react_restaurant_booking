6a82fb6cf23d22d94a29c0beb396a6526380c162
Sergiy Solomka <ssolomka@gmail.com>
2023-04-11 14:55:29 +0200

    Solved day error in console.

--- frontend/src/componentes/Day/Day.tsx
+++ frontend/src/componentes/Day/Day.tsx
@@ -24,46 +24,48 @@ export const Day = () => {
   }, [])
 
 
-  return (<div>
+  return (
+    <div>
       <Navigation />
-      {/*<h3 >{`Bookings of : ${newDateString} `}</h3>*/}
-
-      <div className=" table-responsive" id="day-table">
-      <table className="table table-striped ">
-        <thead>
-        <tr>
-          <th scope="col">DATE</th>
-          <th scope="col">TIME</th>
-          <th scope="col">AMOUNT</th>
-          <th scope="col">NAME</th>
-          <th scope="col">REQUESTS</th>
-          <th scope="col">CONTACT</th>
-          <th scope="col">EDIT</th>
-        </tr>
-        </thead>
-        <tbody>
-        {data
-          ? data
-            .filter((book) => book.date === newDateString)
-            .map((book) => (
-              <tr key={book._id}>
-                <td data-label="DATE">{book.date}</td>
-                <td data-label="TIME">{book.time}</td>
-                <td data-label="AMOUNT">{book.amount}</td>
-                <td data-label="NAME">{book.name}</td>
-                <td data-label="REQUESTS">{book.requests}</td>
-                <td data-label="CONTACT">{book.contact}</td>
-                <td ><button className="btn-outline-primary btn-booking" id={book._id} onClick={()=>editBooking(book._id)}>EDIT</button></td>
-              </tr>
-            ))
-          : ''}
-        </tbody>
-      </table>
-
-      </div >
-
-  </div>
-
-
-  )
+      <div className="table-responsive" id="day-table">
+        <table className="table table-striped">
+          <thead>
+          <tr>
+            <th scope="col">DATE</th>
+            <th scope="col">TIME</th>
+            <th scope="col">AMOUNT</th>
+            <th scope="col">NAME</th>
+            <th scope="col">REQUESTS</th>
+            <th scope="col">CONTACT</th>
+            <th scope="col">EDIT</th>
+          </tr>
+          </thead>
+          <tbody>
+          {data &&
+            data
+              .filter((book) => book.date === newDateString)
+              .map((book) => (
+                <tr key={book._id}>
+                  <td data-label="DATE">{book.date}</td>
+                  <td data-label="TIME">{book.time}</td>
+                  <td data-label="AMOUNT">{book.amount}</td>
+                  <td data-label="NAME">{book.name}</td>
+                  <td data-label="REQUESTS">{book.requests}</td>
+                  <td data-label="CONTACT">{book.contact}</td>
+                  <td>
+                    <button
+                      className="btn-outline-primary btn-booking"
+                      id={book._id}
+                      onClick={() => editBooking(book._id)}
+                    >
+                      EDIT
+                    </button>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
